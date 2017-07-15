fa8541eaed02889f32796b14e6e9f3c2401aebf6
hejiao76 <380791842@qq.com>
2017-07-15 11:52:23 +0800

    test

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -1,27 +1,50 @@
 ]<template>
-  <div class="login">
-    <el-form autoComplete="on" :model="loginForm" :rules="rules" ref="loginForm" label-position="left" class="form">
-      <div class="logo-container">
-        <img class="img" src="../../images/logo.png">
-        <div class="title-container">
-          <h1 class="title">WAF</h1>
-          <h2 class="title-sub">WEB应用防护系统</h2>
+  <div class="global_box">
+    <div class="global_box_top">
+      <div class="nav_top" style="background: #57a126; color: #FFFFFF;">
+        <div class="center_box">
+          您好，欢迎来到家宁装修网
+          <div class="where_box"><span class="iconfont icon-location"></span><span id="city">北京</span><span>【更换】</span></div>
+          <ul class="nav_right">
+            <a>
+              <li>官方微信 </li>
+            </a>
+            <a>
+              <li>工长入驻</li>
+            </a>
+            <a>
+              <li>工长登录</li>
+            </a>
+            <a>
+              <li>北京工装</li>
+            </a>
+            <a>
+              <li>北京家装</li>
+            </a>
+          </ul>
         </div>
       </div>
-      <el-form-item prop="username">
-        <i class="el-icon-edit icon"></i>
-        <el-input name="username" type="text" @keyup.enter.native="handleLogin" v-model="loginForm.username" autoComplete="on" placeholder="用户名">
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <i class="el-icon-edit icon"></i>
-        <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on" placeholder="密   码">
-        </el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="danger" :loading="loading" @click.prevent="handleLogin">登 录</el-button>
-      </el-form-item>
-    </el-form>
+    </div>
+    <!--公用顶部end-->
+    <div class="center_bc">
+      <img class="bc_img_login" src="../../img/login_banner.jpg"  />
+      <div class="login_box">
+        <div class="top_logo"><img src="../../img/logo.png" /> <span>工长登录</span></div>
+        <form action="" method="post">
+          <div class="input_box">
+            <span class="iconfont icon-ordinarymobile"></span>
+            <input v-model="phone" type="text" placeholder="请输入手机号" />
+          </div>
+          <div class="input_box" style="margin-top:20px ;">
+            <span class="iconfont icon-mima"></span>
+            <input v-model="pwd" type="password" placeholder="请输入密码" />
+          </div>
+
+          <a href="#" class="wangji_word">忘记密码?</a>
+          <input  class="btn" v-on:click="handleLogin" type="button" value="登录"/>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,15 +90,16 @@
     },
     methods: {
       handleLogin() {
-        this.$refs.loginForm.validate(valid => {
-          if (valid) {
-            this.loading = true
-            this.$router.push({ path: '/secureMonitor/overview' })
-          } else {
-            console.log('error submit!!')
-            return false
-          }
-        })
+          debugger
+//        this.$refs.loginForm.validate(valid => {
+//          if (valid) {
+//            this.loading = true
+//            this.$router.push({ path: '/secureMonitor/overview' })
+//          } else {
+//            console.log('error submit!!')
+//            return false
+//          }
+//        })
       }
     },
     created() {
@@ -84,71 +108,76 @@
     }
   }
 </script>
-
-<style lang="stylus" rel="stylesheet/stylus" scope>
-  .login
-    height: 100%
-    background-color: #333333
-    .form
-      position: absolute
-      left: 50%
-      top: 40%
-      width: 355px
-      margin-left: -178px
-      margin-top: -180px
-      .logo-container
-        width: 200px
-        height: 100px
-        margin: 0 auto
-        .img
-          width: 60px
-          height: 70px
-          float: left
-        .title-container
-          float: left
-          margin-left: 7px
-          text-align: left
-          .title
-            margin: 10px 0 10px 0
-            font-family: 'Arial Narrow Bold', 'Arial Narrow', 'Arial'
-            font-size: 36px
-            font-weight: 700
-            color: #fff
-          .title-sub
-            color: #9A9DA0
-            font-size: 14px
-      // 输入框下划线
-      .el-form-item
-        border-bottom: 2px solid rgba(255, 255, 255, 0.1)
-        color: #454545
-        .icon
-          position: absolute
-          top: 20px
-          left: 20px
-          z-index: 1
-        // autocomplete填充样式bug修复
-        input:-webkit-autofill,
-        input:-webkit-autofill:hover,
-        input:-webkit-autofill:focus {
-          // 自动填充文本框背景颜色
-          -webkit-box-shadow: 0 0 0px 1000px #333333 inset !important
-          // 自动填充文本框文字颜色
-          -webkit-text-fill-color: #fff !important
-        }
-        input
-          height: 47px
-          padding: 12px 5px 12px 45px
-          background: transparent
-          border: 0px
-          -webkit-appearance: none
-          border-radius: 0px
-          color: #999999
-          font-size: 16px
-          font-weight: 400
-        .el-button
-          width: 100%
-          height: 60px
-          margin-top: 30px
-          font-size: 16px
-          border-radius: 0
+<style>
+  @import "../../css/global.css";
+  @import "../../css/login.css";
+  @import "https://at.alicdn.com/t/font_adkaonfr6xvquxr.css";
 </style>
+
+<!--<style lang="stylus" rel="stylesheet/stylus" scope>-->
+  <!--.login-->
+    <!--height: 100%-->
+    <!--background-color: #333333-->
+    <!--.form-->
+      <!--position: absolute-->
+      <!--left: 50%-->
+      <!--top: 40%-->
+      <!--width: 355px-->
+      <!--margin-left: -178px-->
+      <!--margin-top: -180px-->
+      <!--.logo-container-->
+        <!--width: 200px-->
+        <!--height: 100px-->
+        <!--margin: 0 auto-->
+        <!--.img-->
+          <!--width: 60px-->
+          <!--height: 70px-->
+          <!--float: left-->
+        <!--.title-container-->
+          <!--float: left-->
+          <!--margin-left: 7px-->
+          <!--text-align: left-->
+          <!--.title-->
+            <!--margin: 10px 0 10px 0-->
+            <!--font-family: 'Arial Narrow Bold', 'Arial Narrow', 'Arial'-->
+            <!--font-size: 36px-->
+            <!--font-weight: 700-->
+            <!--color: #fff-->
+          <!--.title-sub-->
+            <!--color: #9A9DA0-->
+            <!--font-size: 14px-->
+      <!--// 输入框下划线-->
+      <!--.el-form-item-->
+        <!--border-bottom: 2px solid rgba(255, 255, 255, 0.1)-->
+        <!--color: #454545-->
+        <!--.icon-->
+          <!--position: absolute-->
+          <!--top: 20px-->
+          <!--left: 20px-->
+          <!--z-index: 1-->
+        <!--// autocomplete填充样式bug修复-->
+        <!--input:-webkit-autofill,-->
+        <!--input:-webkit-autofill:hover,-->
+        <!--input:-webkit-autofill:focus {-->
+          <!--// 自动填充文本框背景颜色-->
+          <!-- -webkit-box-shadow: 0 0 0px 1000px #333333 inset !important-->
+          <!--// 自动填充文本框文字颜色-->
+          <!-- -webkit-text-fill-color: #fff !important-->
+        <!--}-->
+        <!--input-->
+          <!--height: 47px-->
+          <!--padding: 12px 5px 12px 45px-->
+          <!--background: transparent-->
+          <!--border: 0px-->
+          <!-- -webkit-appearance: none-->
+          <!--border-radius: 0px-->
+          <!--color: #999999-->
+          <!--font-size: 16px-->
+          <!--font-weight: 400-->
+        <!--.el-button-->
+          <!--width: 100%-->
+          <!--height: 60px-->
+          <!--margin-top: 30px-->
+          <!--font-size: 16px-->
+          <!--border-radius: 0-->
+<!--</style>-->
